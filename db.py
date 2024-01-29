b00fcda501f80aa6c2e6078ea1340f5e6557170a
IlyaT <treyvish.ilya@gmail.com>
2024-01-29 17:23:08 +0300

    files for github

--- db.py
+++ db.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import soundfile as sf
 import librosa
 import os
+import config
 
 
 async def db_start():
@@ -13,34 +14,37 @@ async def db_start():
 
 
 async def save_photo(message: types.Message, bot: Bot):
-    #await db_start()
-    photo = message.photo[-1]
-    user_id = message.from_user.id
-    user_str = str(user_id)
-    user_path = "photos/" + user_str
-    path = Path(user_path)
-    path.mkdir(parents=True, exist_ok=True)
-    num_files = len(os.listdir(path=path))
-    filename = f'{user_str}_{str(num_files)}.jpg'
-    full_filename = os.path.join(path, filename)
-    await bot.download(photo, destination=full_filename)
+    try:
+        photo = message.photo[-1]
+        user_id = message.from_user.id
+        path = Path(f"videos/{user_id}")
+        path.mkdir(parents=True, exist_ok=True)
+        num_files = len(os.listdir(path=path))
+        filename = f'{user_id}_{str(num_files)}.jpg'
+        full_filename = os.path.join(path, filename)
+        await bot.download(photo, destination=full_filename)
+    except Exception as exc:
+        print(exc)
 
 
 async def save_audio(message: types.Message, bot: Bot):
-    SAMPLERATE = 16000
-    if message.audio:
-        audio = message.audio
-    elif message.voice:
-        audio = message.voice
-    user_id = message.from_user.id
-    user_str = str(user_id)
-    user_path = "audios/" + user_str
-    path = Path(user_path)
-    path.mkdir(parents=True, exist_ok=True)
-    num_files = len(os.listdir(path=path))
-    filename = f'{user_str}_{str(num_files)}.ogg'
-    full_filename = os.path.join(path, filename)
-    await bot.download(audio, destination=full_filename)
-    data, samplerate = librosa.load(full_filename, sr=SAMPLERATE)
-    sf.write(full_filename[:-3] + 'wav', data, SAMPLERATE)
-    os.remove(full_filename)
+    try:
+        necessary_samplerate = config.SAMPLERATE
+        if message.audio:
+            audio = message.audio
+        elif message.voice:
+            audio = message.voice
+        user_id = message.from_user.id
+        path = Path(f"audios/{user_id}")
+        path.mkdir(parents=True, exist_ok=True)
+        num_files = len(os.listdir(path=path))
+        filename = f'{user_id}_{str(num_files)}.ogg'
+        full_filename = os.path.join(path, filename)
+        await bot.download(audio, destination=full_filename)
+        data, samplerate = librosa.load(full_filename, sr=necessary_samplerate)
+        wav_filename = full_filename[:-3] + 'wav'
+        with sf.SoundFile(wav_filename, 'rw') as f:
+            sf.write(data, necessary_samplerate)
+        os.remove(full_filename)
+    except Exception as exc:
+        print(exc)
